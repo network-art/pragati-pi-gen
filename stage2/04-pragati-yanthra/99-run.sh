95a714f34626e69d0662aa7da2a8bb29e39a3ed0
Guruprasad Nagaraj <gpn@networkart.com>
2019-11-01 02:03:47 +0530

    Added boost, poco and eigen3 development packages. Symbolic link to librt from usr/lib.

--- stage2/04-pragati-yanthra/99-run.sh
+++ stage2/04-pragati-yanthra/99-run.sh
@@ -1,1 +1,2 @@
 cd ${ROOTFS_DIR}/lib/arm-linux-gnueabihf && ln -sf librt-2.28.so librt.so
+cd ${ROOTFS_DIR}/usr/lib/arm-linux-gnueabihf && ln -sf ../../../lib/arm-linux-gnueabihf/librt-2.28.so librt.so
