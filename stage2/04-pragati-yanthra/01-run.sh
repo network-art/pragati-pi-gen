8b625d0505bedd852758bfe0f2ee2b5bd4c0f408
Guruprasad Nagaraj <gpn@networkart.com>
2019-11-03 02:03:16 +0530

    Force relative link for ros packages to compile.

--- stage2/04-pragati-yanthra/01-run.sh
+++ stage2/04-pragati-yanthra/01-run.sh
@@ -4,6 +4,7 @@ cd ${ROOTFS_DIR}/lib/arm-linux-gnueabihf && \
 		ln -sf librt-2.28.so librt.so
 
 cd ${ROOTFS_DIR}/usr/lib/arm-linux-gnueabihf && \
+		ln -sf ../../../lib/arm-linux-gnueabihf/libdl.so.2 libdl.so && \
 		ln -sf ../../../lib/arm-linux-gnueabihf/libpng12.so.0 libpng12.so && \
 		ln -sf ../../../lib/arm-linux-gnueabihf/libpng12.so.0 libpng.so && \
 		ln -sf ../../../lib/arm-linux-gnueabihf/libpthread.so.0 libpthread.so && \
