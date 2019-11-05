77262dd4305dfdbe06e66704e912eb8be49f545f
Guruprasad Nagaraj <gpn@networkart.com>
2019-11-05 15:37:12 +0530

    symlinks will not be built until we actually do a pragati yanthra_rel build. Hence, not copying symlinks for now, since we are not actually using symlinks during rootfs build.

--- stage2/04-pragati-yanthra/01-run.sh
+++ stage2/04-pragati-yanthra/01-run.sh
@@ -12,5 +12,3 @@ cd ${ROOTFS_DIR}/usr/lib/arm-linux-gnueabihf && \
 		ln -sf ../../../lib/arm-linux-gnueabihf/libpthread.so.0 libpthread.so && \
 		ln -sf ../../../lib/arm-linux-gnueabihf/librt-2.28.so librt.so && \
 		ln -sf ../../../lib/arm-linux-gnueabihf/libz.so.1.2.11 libz.so
-
-cp -v ${BASE_DIR}/../../third-party/symlinks/symlinks ${ROOTFS_DIR}/usr/bin/
