86cc94160aae6b177a94da44725acbe9b125200d
Guruprasad Nagaraj <gpn@networkart.com>
2019-11-07 00:24:56 +0530

    Setting up ld and environment variables for ros and yanthra.

--- stage2/04-pragati-yanthra/01-run.sh
+++ stage2/04-pragati-yanthra/01-run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+this_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 cd ${ROOTFS_DIR}/lib/arm-linux-gnueabihf && \
 		ln -sf librt-2.28.so librt.so
 
@@ -12,3 +14,18 @@ cd ${ROOTFS_DIR}/usr/lib/arm-linux-gnueabihf && \
 		ln -sf ../../../lib/arm-linux-gnueabihf/libpthread.so.0 libpthread.so && \
 		ln -sf ../../../lib/arm-linux-gnueabihf/librt-2.28.so librt.so && \
 		ln -sf ../../../lib/arm-linux-gnueabihf/libz.so.1.2.11 libz.so
+
+cd ${this_dir}
+install -v -m 644 files/010_pragati-nopasswd "${ROOTFS_DIR}/etc/sudoers.d/"
+install -v -m 644 files/issue                "${ROOTFS_DIR}/etc/"
+install -v -m 644 files/issue.net            "${ROOTFS_DIR}/etc/"
+install -v -m 644 files/ros-profile.sh       "${ROOTFS_DIR}/etc/profile.d/"
+install -v -m 644 files/ros.ld.so.conf       "${ROOTFS_DIR}/etc/ld.so.conf.d/"
+install -v -m 644 files/yanthra-profile.sh   "${ROOTFS_DIR}/etc/profile.d/"
+install -v -m 644 files/yanthra.ld.so.conf   "${ROOTFS_DIR}/etc/ld.so.conf.d/"
+
+echo "network={\
+	ssid=\"GroboMac\"\
+	psk=ac11d7744065093efa44637ebb060637ecf0f74f84386b84213b5fa3f73cedaa\
+}\
+" >> "${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf"
