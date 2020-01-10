971fc70772204c2bf0d84f185cf96381e12f8459
Guruprasad Nagaraj <gpn@networkart.com>
2020-01-10 16:59:17 +0530

    Upgrade distribution only if requested via extra configuration.

--- export-image/01-set-sources/01-run.sh
+++ export-image/01-set-sources/01-run.sh
@@ -2,6 +2,17 @@
 
 on_chroot << EOF
 apt-get update
+EOF
+
+if [ "${DIST_UPGRADE}" = "1" ]; then
+	echo "Performing dist-upgrade"
+	on_chroot << EOF
 apt-get -y dist-upgrade
+EOF
+else
+	echo "dist-upgrade is turned off"
+fi
+
+on_chroot << EOF
 apt-get clean
 EOF
